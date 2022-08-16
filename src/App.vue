71ffbcbc6fb6381db09446cfab2f575bb3bd33b1
Ubong Ntekim <ubongnt@gmail.com>
2022-08-16 11:33:20 +0100

    class 2

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
 <template>
-  <div class="app">Hello Ubdev</div>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('Blingz')">change name</button>
+    <button @click="changeAge(30)">change age</button>
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 
 @Options({
+  name: "App",
   components: {},
+  data() {
+    return {
+      name: "Ubdev",
+      age: 25 as number | string,
+    };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    },
+    changeAge(age: number) {
+      this.age = age;
+    },
+  },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  // [x: string]: any;
+  //   name: any;
+  //   changeName(): void {
+  //     throw new Error("Method not implemented.");
+  //   }
+}
 </script>
 
 <style></style>
